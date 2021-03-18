7f074dcb3716aa450d3d10ad96b7f6f04b67823e
Dafid Prasetyo <dafidpr@gmail.com>
2021-03-18 20:12:05 +0700

    Give permission for delete menu action button

--- resources/views/admin/menu/index.blade.php
+++ resources/views/admin/menu/index.blade.php
@@ -40,18 +40,20 @@
                         <div class="card-inner position-relative card-tools-toggle">
                             <div class="card-title-group">
                                 <div class="card-tools">
-                                    <div class="form-inline flex-nowrap gx-3">
-                                        <div class="form-wrap w-150px">
-                                            <select class="form-select form-select-sm" data-search="off" data-placeholder="Bulk Action">
-                                                <option value="">Bulk Action</option>
-                                                <option value="delete">Delete</option>
-                                            </select>
-                                        </div>
-                                        <div class="btn-wrap">
-                                            <span class="d-none d-md-block"><button class="btn btn-dim btn-outline-light disabled">Apply</button></span>
-                                            <span class="d-md-none"><button class="btn btn-dim btn-outline-light btn-icon disabled"><em class="icon ni ni-arrow-right"></em></button></span>
-                                        </div>
-                                    </div><!-- .form-inline -->
+                                    @can('delete-menus')     
+                                        <div class="form-inline flex-nowrap gx-3">
+                                            <div class="form-wrap w-150px">
+                                                <select class="form-select form-select-sm" data-search="off" data-placeholder="Bulk Action">
+                                                    <option value="">Bulk Action</option>
+                                                    <option value="delete">Delete</option>
+                                                </select>
+                                            </div>
+                                            <div class="btn-wrap">
+                                                <span class="d-none d-md-block"><button class="btn btn-dim btn-outline-light disabled">Apply</button></span>
+                                                <span class="d-md-none"><button class="btn btn-dim btn-outline-light btn-icon disabled"><em class="icon ni ni-arrow-right"></em></button></span>
+                                            </div>
+                                        </div><!-- .form-inline -->
+                                    @endcan
                                 </div><!-- .card-tools -->
                             </div><!-- .card-title-group -->
                         </div><!-- .card-inner -->
